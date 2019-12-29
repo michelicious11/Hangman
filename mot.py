4e9b9fb50be07edc7c0e7483d94dd059361fc09b
Nictaclope <46601329+Nictaclope@users.noreply.github.com>
2019-12-29 18:37:24 -0500

    Add files via upload

---
+++ mot.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 27 09:11:37 2019
+
+@author: Mick Pl
+"""
+
+class Mot:
+    def __init__(self, interface):
+        self.interface = interface
+        
+        
+    def check_word():
+        
+    
+    def remove_letter():
+        
+        
+    def afficher_letter():
+        
