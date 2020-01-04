6770ec9e7c7f103cfa699c8e94a0a43acc42a068
Alar31000 <46691211+Alar31000@users.noreply.github.com>
2020-01-03 20:32:15 -0500

    mot.py
    Ajout de : remove_letter, check_word et afficher_letter

--- mot.py
+++ mot.py
@@ -14,13 +14,23 @@ class Mot:
         
         
     def check_word():
-        
+	global word
+	x = len(word)
+	print("word length is: ", x)
+	for x in word:
+		print("-")
     
-    def remove_letter():
+    def remove_letter(letter):
+        alphabet = list(string.ascii.undercase)
+        if letter in alphabet:
+           alphabet.remove(letter)
+        else:
+           print("Already removed!")
+	
+        return alphabet
         
-        
-    def afficher_letter():
-   
+    def afficher_letter(letter):
+        print("The Selected letter is :", letter)
 
     def pick_random_word():
 	datapath = '/WordDB50.xlsx'
